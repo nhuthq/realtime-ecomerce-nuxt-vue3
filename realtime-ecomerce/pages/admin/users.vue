35b89df88d78cfed93f183c48e9b1d54af75c990
nhuthq <nhuthq@zigexn.vn>
2025-07-25 11:00:27 +0700

    init admin layout

---
+++ realtime-ecomerce/pages/admin/users.vue
@@ -0,0 +1,15 @@
+<script  setup>
+  
+  definePageMeta({
+      layout: 'admin'
+  })
+  
+  </script>
+
+<template>
+    <div class="bg-slate-200 h-screen w-screen">
+      Users
+    </div>
+  </template>
+  
+  
